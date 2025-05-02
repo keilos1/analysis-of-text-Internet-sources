33009800f7684eb24b6fa933229ef89da09d5573
irishavrednisha <154934567+irishavrednisha@users.noreply.github.com>
2025-05-02 17:59:43 +0300

    Update app.py

--- app/web_interface/app.py
+++ app/web_interface/app.py
@@ -5,8 +5,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import FileResponse
 
-from ..data_collection.socialScraper import SocialScraper
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
