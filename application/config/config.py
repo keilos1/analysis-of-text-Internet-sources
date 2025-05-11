ca95428012072544291d6d76bb05f533ed67c2c9
kekos <keoks12@mail.ru>
2025-05-11 18:12:42 +0300

    config files

---
+++ application/config/config.py
@@ -0,0 +1,13 @@
+# Параметры подключения
+HOST = '78.36.44.126'
+PORT = 57381
+SSH_USER = 'server'
+SSH_PASSWORD = 'tppo'
+
+# Параметры MongoDB
+DB_NAME = 'newsPTZ'
+MONGO_HOST = '127.0.0.1'
+MONGO_PORT = 27017
+
+# Параметры подключения сайта
+SITE_HOST = '78.36.44.126'
